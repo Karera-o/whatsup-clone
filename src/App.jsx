4f033e1b99762e79ae184c57f7c305c7d96e307a
karera-o <olivierkarera2020@gmail.com>
2022-07-21 21:26:01 +0200

    Adding icons in the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import './AppResponsive.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div id="app_leftSide">
+        
+      </div>
+      <div id="app_mainSide">
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/components/leftSide/header/leftSideHeader.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import {TbCircleDashed,TbMessage} from "react-icons/tb";
+import {BsThreeDotsVertical,BsFilter} from "react-icons/bs";
+import {IoIosSearch,IoIosNotificationsOff,IoCloseOutline} from "react-icons/io";
+import {AiOutlineArrowLeft} from "react-icons/ai";
+ 
+export class LeftSideHeader extends Component{
+
+    render(){
+        return(){
+            <div>
+            </div>
+        }
+    }
+}
